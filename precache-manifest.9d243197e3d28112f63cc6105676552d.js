eee638fabf7259aef404d09b1489b392a42f4243
kevalriya <patelkeval123@gmail.com>
2019-10-23 13:39:39 -0400

    Updates

---
+++ precache-manifest.9d243197e3d28112f63cc6105676552d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd548ac219b24610569537dfac26539",
+    "url": "/driver-profile/index.html"
+  },
+  {
+    "revision": "68c2bb93b4c5d897f381",
+    "url": "/driver-profile/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "f2ff206eec97aa1cec02",
+    "url": "/driver-profile/static/css/main.1dc9e4e9.chunk.css"
+  },
+  {
+    "revision": "68c2bb93b4c5d897f381",
+    "url": "/driver-profile/static/js/2.3dbc620b.chunk.js"
+  },
+  {
+    "revision": "f2ff206eec97aa1cec02",
+    "url": "/driver-profile/static/js/main.1fc4832d.chunk.js"
+  },
+  {
+    "revision": "c75dbb3f7152bc472d72",
+    "url": "/driver-profile/static/js/runtime~main.3182f477.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/driver-profile/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
